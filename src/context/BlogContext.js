d0555ec903cbd2196d4f67c14c8abb00dfeb3127
yoonpyaepyaenyein <yoonpyae1310@gmail.com>
2022-06-19 19:13:18 +0630

    Automating Context Creation

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from "react";
 
-const BlogContext = React.createContext();
+import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
@@ -11,18 +11,12 @@ const blogReducer = (state, action) => {
   }
 };
 
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, dispatch] = useReducer(blogReducer, []);
-
-  const addBlogPost = () => {
-    dispatch({ type: "add_blogpost" });
-  };
-
-  return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
-      {children}
-    </BlogContext.Provider>
-  );
+const addBlogPost = () => {
+  dispatch({ type: "add_blogpost" });
 };
 
-export default BlogContext;
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  []
+);
